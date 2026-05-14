8a860928108b7d96842829ca5f4c3537b39ea013
nbry <nwbryant@umich.edu>
2026-05-14 15:46:39 -0700

    feat: application boilerplate

--- src/nbry_lifting_website/app.py
+++ src/nbry_lifting_website/app.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from python-project-template!")
+from pathlib import Path
+from fastapi import FastAPI
+
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory=str(Path(__file__).parent / "templates"))
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="home.html", context={"request": request}
+    )
