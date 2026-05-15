ef4147a1fdfa78a38f6389846b1c7e2b2e147e4c
nbry <nwbryant@umich.edu>
2026-05-14 20:52:46 -0700

    feat: faq, home, instructions template bp

--- src/nbry_lifting_website/app.py
+++ src/nbry_lifting_website/app.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from fastapi import FastAPI
 
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
 
 app = FastAPI()
 
@@ -11,6 +10,23 @@ templates = Jinja2Templates(directory=str(Path(__file__).parent / "templates"))
 
 @app.get("/")
 async def home(request: Request):
+    """Home page of the website."""
     return templates.TemplateResponse(
         request=request, name="home.html", context={"request": request}
+    )
+
+
+@app.get("/instructions")
+async def instructions(request: Request):
+    """Instructions page of the website."""
+    return templates.TemplateResponse(
+        request=request, name="instructions.html", context={"request": request}
+    )
+
+
+@app.get("/faq")
+async def faq(request: Request):
+    """FAQ page of the website."""
+    return templates.TemplateResponse(
+        request=request, name="faq.html", context={"request": request}
     )
